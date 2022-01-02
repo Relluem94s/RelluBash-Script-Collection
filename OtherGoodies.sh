ea0fc58e25776e7db3b336550b391b7dd414ea0d
Relluem94 <github@relluem94.de>
2022-01-02 13:17:59 +0100

    added ovpn networkmanager to goodies

---
+++ OtherGoodies.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+
+###########################################################################################
+#                                    Commandline Goodies                                  #
+#                                      by Relluem94 2022                                  #
+###########################################################################################
+#                                                                                         #
+# Requires nmcli to run!                                                                  #  
+# Sudo required for some operations!
+#                                                                                         #
+###########################################################################################
+#                                     DO NOT TOUCH THE CODE                               #
+###########################################################################################
+#
+#
+#
+options=("Add OVPN File via Network Manager" "Open Port in IP Tables" "Exit")
+select opt in "${options[@]}"
+
+do
+    case $opt in
+        "Add OVPN File via Network Manager")
+        
+            ###########################################################################
+            read client
+            sudo nmcli connection import type openvpn file $client.ovpn
+            ###########################################################################
+            
+            break
+            ;;
+            "Open Port in IP Tables")
+        
+            ###########################################################################
+            read port
+            sudo nmcli connection import type openvpn file $client.ovpn
+            ###########################################################################
+            
+            break
+            ;;
+        "Exit")
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
+
+
+
+
+
