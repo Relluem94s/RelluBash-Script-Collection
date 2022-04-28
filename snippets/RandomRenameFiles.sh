bbdeb858832960bf5485f43dded747c021848363
Relluem94 <github@relluem94.de>
2022-04-28 21:27:00 +0200

    updated script

--- snippets/RandomRenameFiles.sh
+++ snippets/RandomRenameFiles.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+###########################################################################################
+#                                     Random Rename Files                                 #
+#                                  by Relluem94 2021 - 2022                               #
+###########################################################################################
+# Parameter 1 is the Directory
+# Parameter 2 is the Extension for the files
+#
+#
+#
 if [ -n "$1" ]; then
 	echo "Change Directory to $1"
 	cd "$1"
 	
 	if [ -n "$2" ]; then
 		for f in *.$2; 
-			do # mv "$f" $RANDOM-"$f"; 
+			do mv "$f" $RANDOM-"$f"; 
 			echo "Moving $f to" $RANDOM-"$f";
 		done
 	else
@@ -15,3 +24,9 @@ if [ -n "$1" ]; then
 else 
 	echo "Parameter one (Directory) is missing"
 fi
+#
+#
+#
+###########################################################################################
+#                                           END OF CODE                                   #
+###########################################################################################
