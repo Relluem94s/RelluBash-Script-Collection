cf76fa5db2d91c436ed5fb50d708a6887971164f
Relluem94 <github@relluem94.de>
2022-11-02 22:53:00 +0100

    adjusts

--- snippets/fl_studio_installer_fedora.sh
+++ snippets/fl_studio_installer_fedora.sh
@@ -3,7 +3,12 @@
 sudo dnf update --refresh -y
 sudo dnf upgrade --refresh -y
 
-sudo dnf install wine winetricks
+sudo dnf -y install dnf-plugins-core
+sudo dnf config-manager --add-repo https://dl.winehq.org/wine-builds/fedora/35/winehq.repo
+
+sudo dnf -y install winehq-stable
+
+sudo dnf install winetricks
 
 wget https://demodownload.image-line.com/flstudio/flstudio_win64_20.9.2.2963.exe -P /tmp/fl_studio
 
@@ -15,3 +20,5 @@ echo "${files[0]}"
 wine ${files[0]}
 
 rm -rf /tmp/fl_studio
+
+
