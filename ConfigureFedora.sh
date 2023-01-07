20f90ad2e2e6de7a731e850727a24e4d9d00cb1d
Relluem94 <github@relluem94.de>
2023-01-07 18:12:41 +0100

    Update ConfigureFedora.sh

--- ConfigureFedora.sh
+++ ConfigureFedora.sh
@@ -67,9 +67,9 @@ sudoedit /etc/gdm/custom.conf
 
 
 #Docker
-sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin
+sudo dnf remove docker docker-client docker-client-latest docker-common docker-latest docker-latest-logrotate docker-logrotate docker-selinux docker-engine-selinux docker-engine -y 
+sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo -y
+sudo dnf install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 
 sudo systemctl start docker
 sudo systemctl enable docker
