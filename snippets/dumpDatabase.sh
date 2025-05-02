826064065c676276caa91dbe3cd26619845a473c
Relluem94 <github@relluem94.de>
2025-05-02 22:44:45 +0200

    update DumpScript

--- snippets/dumpDatabase.sh
+++ snippets/dumpDatabase.sh
@@ -9,4 +9,39 @@ DB_NAME="$1"
 TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
 FILENAME="${DB_NAME}_${TIMESTAMP}.sql"
 
-mysqldump -u root -p "$DB_NAME" > "$FILENAME"
+MYSQL_HOST="$SERVER_MYSQL_HOST_LOCALHOST"
+MYSQL_USER="$SERVER_MYSQL_USER_LOCALHOST"
+MYSQL_PASSWORD="$SERVER_MYSQL_PASSWORD_LOCALHOST"
+
+
+
+if [ -z "$MYSQL_HOST" ] || [ -z "$MYSQL_USER" ] || [ -z "$MYSQL_PASSWORD" ]; then
+    echo "Error: One or more required variables (MYSQL_HOST, MYSQL_USER, MYSQL_PASSWORD) not set in .secrets"
+    exit 1
+fi
+
+# Creating Temp Config
+TEMP_CNF=$(mktemp)
+cat > "$TEMP_CNF" <<EOL
+[client]
+host=$MYSQL_HOST
+user=$MYSQL_USER
+password=$MYSQL_PASSWORD
+EOL
+
+chmod 600 "$TEMP_CNF"
+
+
+# Starting Dump
+mysqldump --defaults-extra-file="$TEMP_CNF" "$DB_NAME" > "$FILENAME"
+
+
+if [ $? -eq 0 ]; then
+    echo "Backup successfully created: $FILENAME"
+else
+    echo "Error creating backup"
+    rm -f "$TEMP_CNF"
+    exit 1
+fi
+
+rm -f "$TEMP_CNF"
