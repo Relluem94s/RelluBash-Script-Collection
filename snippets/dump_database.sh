80012c4d050d53f51c09a078506e71e536c504d7
Relluem94 <github@relluem94.de>
2025-05-02 16:57:50 +0200

    updated dump Script

--- snippets/dump_database.sh
+++ snippets/dump_database.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-mysqldump -u root -p rellu_essentials > rellu_essentials.sql
+if [ -z "$1" ]; then
+    echo "Usage: $0 <database_name>"
+    exit 1
+fi
+
+DB_NAME="$1"
+TIMESTAMP=$(date +"%Y-%m-%d_%H-%M")
+FILENAME="${DB_NAME}_${TIMESTAMP}.sql"
+
+mysqldump -u root -p "$DB_NAME" > "$FILENAME"
